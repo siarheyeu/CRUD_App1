b7ba74c81bebe9d593c672f669d5932fcff4b051
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2022-12-26 23:52:28 +0300

    Validation of forms 2

--- src/main/java/ru/siarheyeu/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/siarheyeu/springcourse/controllers/PeopleController.java
@@ -49,7 +49,11 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id){
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+                         @PathVariable("id") int id){
+        if (bindingResult.hasErrors())
+            return "people/edit";
+
         personDAO.update(id, person);
         return "redirect:/people";
     }
