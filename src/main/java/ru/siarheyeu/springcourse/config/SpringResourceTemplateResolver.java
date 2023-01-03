ea05858e968e281d320e2a0c16859f761d2c0ce8
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-01-03 12:32:46 +0300

    Database configuration from an external file

---
+++ src/main/java/ru/siarheyeu/springcourse/config/SpringResourceTemplateResolver.java
@@ -0,0 +1,14 @@
+package ru.siarheyeu.springcourse.config;
+
+import org.springframework.context.ApplicationContext;
+
+public class SpringResourceTemplateResolver {
+    public void setApplicationContext(ApplicationContext applicationContext) {
+    }
+
+    public void setPrefix(String s) {
+    }
+
+    public void setSuffix(String s) {
+    }
+}
