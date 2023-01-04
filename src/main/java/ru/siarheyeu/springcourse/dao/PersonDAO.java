9fa1001365c636061d4ee3e3653453c8cca40703
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-01-04 18:48:21 +0300

    Сascading in databases

--- src/main/java/ru/siarheyeu/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/siarheyeu/springcourse/dao/PersonDAO.java
@@ -31,7 +31,7 @@ public class PersonDAO {
                 .stream().findAny().orElse(null);
     }
         public void save(Person person) {
-            jdbcTemplate.update("INSERT INTO Person VALUES(1, ?, ?,?)", person.getName(), person.getAge(), person.getEmail());
+            jdbcTemplate.update("INSERT INTO Person(name, age, email) VALUES(?, ?,?)", person.getName(), person.getAge(), person.getEmail());
 
         }
 
