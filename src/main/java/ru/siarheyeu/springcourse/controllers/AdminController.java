f9c85bc776bf41813906f335fc677c57db69f6a7
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-01-09 14:30:03 +0300

    Drop-down lists 2

--- src/main/java/ru/siarheyeu/springcourse/controllers/AdminController.java
+++ src/main/java/ru/siarheyeu/springcourse/controllers/AdminController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.siarheyeu.springcourse.dao.PersonDAO;
 import ru.siarheyeu.springcourse.models.Person;
@@ -22,7 +23,15 @@ public class AdminController {
 
     @GetMapping()
     public String adminPage(Model model, @ModelAttribute("person") Person person){
+        model.addAttribute("people", personDAO.index());
 
+        return "adminPage";
     }
 
+    @PatchMapping("/add")
+    public String makeAdmin(@ModelAttribute("person") Person person) {
+        System.out.println(person.getId());
+
+        return "redirect:/people";
+    }
 }
